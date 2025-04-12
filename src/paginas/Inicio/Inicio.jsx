5493d5b7cf4f725abcf7df18ec29a7f5316638a7
victorjose20 <victoraraujo@unigranrio.br>
2025-04-11 23:29:40 -0300

    mudança no link do linkedin

--- src/paginas/Inicio/Inicio.jsx
+++ src/paginas/Inicio/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () =>{
             <h1 className="text-white animate-slideIn // max-ce:text-4xl /// max-xs:text-2xl">Olá eu sou Victor José</h1>
             <h1 className="text-white animate-slideIn // max-ce:text-4xl /// max-xs:text-2xl">E sou um desenvolvedor Frontend</h1>
             <div  className="flex flex-row mt-5 gap-5">
-                <a target="_blank" href="https://www.linkedin.com/in/victor-araujo-desenvolvedor-frontend"> <img className="border-solid rounded-full bg-white" src={link} alt="" /> </a>
+                <a target="_blank" href="https://www.linkedin.com/in/victor-josé-araujo"> <img className="border-solid rounded-full bg-white" src={link} alt="" /> </a>
                 <a target="_blank"href="https://github.com/victorjose20  "> <img className="border-solid rounded-full bg-white" src={github} alt="" />  </a>
             </div>
             
